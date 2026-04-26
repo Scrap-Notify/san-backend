b169674e6ff33da95d90ee21e96c91523a24cd5d
dahyeon <youlove7r@gmail.com>
2026-04-26 23:45:13 +0900

    chore(init): Spring Boot 초기 세팅 (S14P31A309-53)

---
+++ src/test/java/com/san/api/SanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

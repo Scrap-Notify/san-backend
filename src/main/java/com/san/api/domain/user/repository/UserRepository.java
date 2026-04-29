19b17c3901c0852493ba4e8588647457688a5818
Dahyeonni <youlove7r@gmail.com>
2026-04-29 14:55:07 +0900

    feat(user): User에 GitHub provider 식별자 추가 (S14P31A309-87)

--- src/main/java/com/san/api/domain/user/repository/UserRepository.java
+++ src/main/java/com/san/api/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.san.api.domain.user.repository;
 
+import com.san.api.domain.user.entity.AuthProvider;
 import com.san.api.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +10,8 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
 
     boolean existsByUsername(String username);
 }

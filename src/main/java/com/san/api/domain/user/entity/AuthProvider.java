a87d611d8f87f9a4a986cd3414af4b4476ddaced
DESKTOP-6OO0P6F\SSAFY <ajjoona@gmail.com>
2026-05-07 09:45:49 +0900

    refactor(user): enum 주석을 description 필드로 통일 (S14P31A309-133)

--- src/main/java/com/san/api/domain/user/entity/AuthProvider.java
+++ src/main/java/com/san/api/domain/user/entity/AuthProvider.java
@@ -1,7 +1,13 @@
 package com.san.api.domain.user.entity;
 
-/** 인증 제공자 유형. LOCAL은 자체 회원가입, GITHUB은 OAuth2 소셜 로그인. */
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum AuthProvider {
-    LOCAL,
-    GITHUB
+    LOCAL("자체 회원가입"),
+    GITHUB("OAuth2 소셜 로그인");
+
+    private final String description;
 }

36e9e64edc30764b1b4fba9136eb779ca6ae2cdb
K-DaHee <dhk086512@gmail.com>
2026-05-19 12:56:49 +0900

    feat(recall): 날짜 기반 리콜 퀴즈 생성 서비스 구현 (S14P31A309-295)

--- src/main/java/com/san/api/global/exception/errorcode/RecallErrorCode.java
+++ src/main/java/com/san/api/global/exception/errorcode/RecallErrorCode.java
@@ -11,6 +11,7 @@ public enum RecallErrorCode implements ErrorCode {
 
     RECALL_TIL_NOT_FOUND(HttpStatus.NOT_FOUND, "R001", "리콜 퀴즈를 생성할 TIL이 없습니다."),
     EMPTY_RECALL_SOURCE(HttpStatus.BAD_REQUEST, "R002", "리콜 퀴즈를 생성할 원본이 없습니다."),
+    INVALID_RECALL_SOURCE_CONTENT(HttpStatus.BAD_REQUEST, "R003", "리콜 퀴즈를 생성할 원본 내용이 올바르지 않습니다."),
     ;
 
     private final HttpStatus status;
